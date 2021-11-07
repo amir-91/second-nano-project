067f530b89c206fd0ba9c17221b80f055e275e85
amir-91 <61394176+amir-91@users.noreply.github.com>
2021-11-08 00:14:46 +0200

    re-submitting

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # second-nano-project
- 
+
+this website is created by html css javascript.
+it talks about awesome places and some infromation about it.
+
--- js/app.js
+++ js/app.js
@@ -1,6 +1,55 @@
 const date = document.getElementsByClassName('date')
 const anc = document.querySelectorAll('.anc')
+const nav = document.getElementById('nav-bar')
 const dateData = [...date]
+
+//dynamic navbar
+
+var content = document.createElement('div');
+	content.innerHTML = "";
+	var liststart = document.createElement('ul');
+	var listelement1 = document.createElement('li');
+	var listelement2 = document.createElement('li');
+  var listelement3 = document.createElement('li');
+  var listelement4 = document.createElement('li');
+
+  listelement1.classList = 'sec-1'
+  listelement2.classList = 'sec-2'
+  listelement3.classList = 'sec-3'
+  listelement4.classList = 'sec-4'
+
+
+	listelement1.innerHTML = '<a href="#sec-1">section 1</a>';
+	listelement2.innerHTML = '<a href="#sec-2">section 2</a>';
+  listelement3.innerHTML = '<a href="#sec-3">section 3</a>';
+  listelement4.innerHTML = '<a href="#sec-4">section 4</a>';
+
+	liststart.appendChild(listelement1);
+	liststart.appendChild(listelement2);
+  liststart.appendChild(listelement3);
+  liststart.appendChild(listelement4);
+
+
+	nav.appendChild(liststart);
+ 
+  // navigate to section dynamically
+
+document.querySelector('#sec-1').addEventListener('click', function() {
+  document.querySelector('#sec-1').scrollIntoView();
+});
+
+document.querySelector('#sec-2').addEventListener('click', function() {
+  document.querySelector('#sec-2').scrollIntoView();
+});
+
+document.querySelector('#sec-3').addEventListener('click', function() {
+  document.querySelector('#sec-3').scrollIntoView();
+});
+
+document.querySelector('#sec-4').addEventListener('click', function() {
+  document.querySelector('#sec-4').scrollIntoView();
+});
+	
 
  anc.forEach((item) => {
   item.addEventListener('click', (e) => {
@@ -30,12 +79,12 @@ const dateData = [...date]
 
 
 const sections = document.querySelectorAll("section")
-const navLi = document.querySelectorAll("nav .container ul li")
+const navLi = document.querySelectorAll(".container ul li")
 window.onscroll = () => {
   let current = ""
   sections.forEach((section) => {
     const sectionTop = section.offsetTop
-    if (pageYOffset >= sectionTop-200) {
+    if (pageYOffset >= sectionTop-300) {
       current = section.getAttribute("id")
      }
   });
@@ -47,3 +96,4 @@ window.onscroll = () => {
     }
   });
 };
+
